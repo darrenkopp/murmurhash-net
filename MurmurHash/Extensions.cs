6594e182ab9856e18baff5675abf8723a17bdbe2
Darren Kopp <darrenkopp@gmail.com>
2013-06-18 21:04:49 -0600

    namespace cleanup

--- MurmurHash/Extensions.cs
+++ MurmurHash/Extensions.cs
@@ -12,12 +12,7 @@
 /// See the License for the specific language governing permissions and
 /// limitations under the License.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Murmur
 {
