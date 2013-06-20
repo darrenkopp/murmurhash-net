da7a20641035f1c1f5836021d5692e836060e9e5
Darren Kopp <darrenkopp@gmail.com>
2013-06-19 21:48:42 -0600

    optimize and partial
    optimize algorithm
    support partial hashing

--- MurmurHash/Unmanaged/Murmur128UnmanagedX64.cs
+++ MurmurHash/Unmanaged/Murmur128UnmanagedX64.cs
@@ -18,8 +18,8 @@ namespace Murmur
 {
     internal class Murmur128UnmanagedX64 : Murmur128
     {
-        const ulong C1 = 0x87c37b91114253d5L;
-        const ulong C2 = 0x4cf5ad432745937fL;
+        const ulong C1 = 0x87c37b91114253d5UL;
+        const ulong C2 = 0x4cf5ad432745937fUL;
 
         internal Murmur128UnmanagedX64(uint seed = 0)
             : base(seed)
@@ -45,38 +45,35 @@ namespace Murmur
 
             // only compute the hash if we have data to hash
             if (cbSize > 0)
-            {
-                unsafe
-                {
-                    // grab pointer to first byte in array
-                    fixed (byte* data = array)
-                        Body(data, cbSize);
-                }
-            }
+                Body(array, ibStart, cbSize);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        unsafe private void Body(byte* data, int length)
+        private void Body(byte[] data, int start, int length)
         {
-            int remaining = length;
-            int position = 0;
-            ulong* current = (ulong*)data;
+            int remainder = length & 15;
+            int blocks = length / 16;
 
-            while (remaining >= 16)
+            unsafe
             {
-                ulong k1 = *current++, k2 = *current++;
-                remaining -= 16; position += 16;
+                fixed (byte* d = &data[start])
+                {
+                    ulong* current = (ulong*)d;
 
-                // a variant of original algorithm optimized for processor instruction pipelining
-                H1 = H1 ^ ((k1 * C1).RotateLeft(31) * C2);
-                H1 = (H1.RotateLeft(27) + H2) * 5 + 0x52dce729;
+                    while (blocks-- > 0)
+                    {
+                        // a variant of original algorithm optimized for processor instruction pipelining
+                        H1 = H1 ^ ((*current++ * C1).RotateLeft(31) * C2);
+                        H1 = (H1.RotateLeft(27) + H2) * 5 + 0x52dce729;
 
-                H2 = H2 ^ ((k2 * C2).RotateLeft(33) * C1);
-                H2 = (H2.RotateLeft(31) + H1) * 5 + 0x38495ab5;
+                        H2 = H2 ^ ((*current++ * C2).RotateLeft(33) * C1);
+                        H2 = (H2.RotateLeft(31) + H1) * 5 + 0x38495ab5;
+                    }
+
+                    if (remainder > 0)
+                        Tail(d, (length - remainder), remainder);
+                }
             }
-
-            if (remaining > 0)
-                Tail(data, position, remaining);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -88,21 +85,21 @@ namespace Murmur
             // determine how many bytes we have left to work with based on length
             switch (remaining)
             {
-                case 15: k2 ^= (ulong)tail[start + 14] << 48; goto case 14;
-                case 14: k2 ^= (ulong)tail[start + 13] << 40; goto case 13;
-                case 13: k2 ^= (ulong)tail[start + 12] << 32; goto case 12;
-                case 12: k2 ^= (ulong)tail[start + 11] << 24; goto case 11;
-                case 11: k2 ^= (ulong)tail[start + 10] << 16; goto case 10;
-                case 10: k2 ^= (ulong)tail[start + 9] << 8; goto case 9;
-                case 9: k2 ^= (ulong)tail[start + 8] << 0; goto case 8;
-                case 8: k1 ^= (ulong)tail[start + 7] << 56; goto case 7;
-                case 7: k1 ^= (ulong)tail[start + 6] << 48; goto case 6;
-                case 6: k1 ^= (ulong)tail[start + 5] << 40; goto case 5;
-                case 5: k1 ^= (ulong)tail[start + 4] << 32; goto case 4;
-                case 4: k1 ^= (ulong)tail[start + 3] << 24; goto case 3;
-                case 3: k1 ^= (ulong)tail[start + 2] << 16; goto case 2;
-                case 2: k1 ^= (ulong)tail[start + 1] << 8; goto case 1;
-                case 1: k1 ^= (ulong)tail[start] << 0; break;
+                case 15: k2 ^= (ulong)tail[start + 14] << 48;   goto case 14;
+                case 14: k2 ^= (ulong)tail[start + 13] << 40;   goto case 13;
+                case 13: k2 ^= (ulong)tail[start + 12] << 32;   goto case 12;
+                case 12: k2 ^= (ulong)tail[start + 11] << 24;   goto case 11;
+                case 11: k2 ^= (ulong)tail[start + 10] << 16;   goto case 10;
+                case 10: k2 ^= (ulong)tail[start + 9] << 8;     goto case 9;
+                case 9: k2 ^= (ulong)tail[start + 8] << 0;      goto case 8;
+                case 8: k1 ^= (ulong)tail[start + 7] << 56;     goto case 7;
+                case 7: k1 ^= (ulong)tail[start + 6] << 48;     goto case 6;
+                case 6: k1 ^= (ulong)tail[start + 5] << 40;     goto case 5;
+                case 5: k1 ^= (ulong)tail[start + 4] << 32;     goto case 4;
+                case 4: k1 ^= (ulong)tail[start + 3] << 24;     goto case 3;
+                case 3: k1 ^= (ulong)tail[start + 2] << 16;     goto case 2;
+                case 2: k1 ^= (ulong)tail[start + 1] << 8;      goto case 1;
+                case 1: k1 ^= (ulong)tail[start] << 0;          break;
             }
 
             H2 = H2 ^ ((k2 * C2).RotateLeft(33) * C1);
