a56eff646c0e88e28fc4ba69a284878b1c7767c0
Darren Kopp <darrenkopp@gmail.com>
2013-06-19 21:59:58 -0600

    performance
    ever so slightly faster

--- MurmurHash/Unmanaged/Murmur128UnmanagedX64.cs
+++ MurmurHash/Unmanaged/Murmur128UnmanagedX64.cs
@@ -71,13 +71,13 @@ namespace Murmur
                     }
 
                     if (remainder > 0)
-                        Tail(d, (length - remainder), remainder);
+                        Tail(d + (length - remainder), remainder);
                 }
             }
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        unsafe private void Tail(byte* tail, int start, int remaining)
+        unsafe private void Tail(byte* tail, int remaining)
         {
             // create our keys and initialize to 0
             ulong k1 = 0, k2 = 0;
@@ -85,21 +85,21 @@ namespace Murmur
             // determine how many bytes we have left to work with based on length
             switch (remaining)
             {
-                case 15: k2 ^= (ulong)tail[start + 14] << 48;   goto case 14;
-                case 14: k2 ^= (ulong)tail[start + 13] << 40;   goto case 13;
-                case 13: k2 ^= (ulong)tail[start + 12] << 32;   goto case 12;
-                case 12: k2 ^= (ulong)tail[start + 11] << 24;   goto case 11;
-                case 11: k2 ^= (ulong)tail[start + 10] << 16;   goto case 10;
-                case 10: k2 ^= (ulong)tail[start + 9] << 8;     goto case 9;
-                case 9: k2 ^= (ulong)tail[start + 8] << 0;      goto case 8;
-                case 8: k1 ^= (ulong)tail[start + 7] << 56;     goto case 7;
-                case 7: k1 ^= (ulong)tail[start + 6] << 48;     goto case 6;
-                case 6: k1 ^= (ulong)tail[start + 5] << 40;     goto case 5;
-                case 5: k1 ^= (ulong)tail[start + 4] << 32;     goto case 4;
-                case 4: k1 ^= (ulong)tail[start + 3] << 24;     goto case 3;
-                case 3: k1 ^= (ulong)tail[start + 2] << 16;     goto case 2;
-                case 2: k1 ^= (ulong)tail[start + 1] << 8;      goto case 1;
-                case 1: k1 ^= (ulong)tail[start] << 0;          break;
+                case 15: k2 ^= (ulong)tail[14] << 48;   goto case 14;
+                case 14: k2 ^= (ulong)tail[13] << 40;   goto case 13;
+                case 13: k2 ^= (ulong)tail[12] << 32;   goto case 12;
+                case 12: k2 ^= (ulong)tail[11] << 24;   goto case 11;
+                case 11: k2 ^= (ulong)tail[10] << 16;   goto case 10;
+                case 10: k2 ^= (ulong)tail[9] << 8;     goto case 9;
+                case 9: k2 ^= (ulong)tail[8] << 0;      goto case 8;
+                case 8: k1 ^= (ulong)tail[7] << 56;     goto case 7;
+                case 7: k1 ^= (ulong)tail[6] << 48;     goto case 6;
+                case 6: k1 ^= (ulong)tail[5] << 40;     goto case 5;
+                case 5: k1 ^= (ulong)tail[4] << 32;     goto case 4;
+                case 4: k1 ^= (ulong)tail[3] << 24;     goto case 3;
+                case 3: k1 ^= (ulong)tail[2] << 16;     goto case 2;
+                case 2: k1 ^= (ulong)tail[1] << 8;      goto case 1;
+                case 1: k1 ^= (ulong)tail[0] << 0;      break;
             }
 
             H2 = H2 ^ ((k2 * C2).RotateLeft(33) * C1);
