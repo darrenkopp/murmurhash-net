a3a0e41ce790936e56374057ab319e96f294a41a
Darren Kopp <darrenkopp@gmail.com>
2013-06-20 19:08:28 -0600

    support picking specific algorithm implementation
    add preference to pick between x64 and x86, or auto selecting optimal
    algorithm

--- MurmurHash/MurmurHash.cs
+++ MurmurHash/MurmurHash.cs
@@ -17,6 +17,13 @@ using System.Security.Cryptography;
 
 namespace Murmur
 {
+    public enum AlgorithmPreference
+    {
+        Auto,
+        X64,
+        X86
+    }
+
     public static class MurmurHash
     {
         public static Murmur32 Create32(uint seed = 0, bool managed = true)
@@ -27,23 +34,31 @@ namespace Murmur
             return new Murmur32UnmanagedX86(seed);
         }
 
-        public static Murmur128 Create128(uint seed = 0, bool managed = true)
+        public static Murmur128 Create128(uint seed = 0, bool managed = true, AlgorithmPreference preference = AlgorithmPreference.Auto)
         {
             var algorithm = managed
-                ? Pick(seed, s => new Murmur128ManagedX86(s), s => new Murmur128ManagedX64(s))
-                : Pick(seed, s => new Murmur128UnmanagedX86(s), s => new Murmur128UnmanagedX64(s));
+                ? Pick(seed, preference, s => new Murmur128ManagedX86(s), s => new Murmur128ManagedX64(s))
+                : Pick(seed, preference, s => new Murmur128UnmanagedX86(s), s => new Murmur128UnmanagedX64(s));
 
             return algorithm as Murmur128;
         }
 
-        private static HashAlgorithm Pick<T32, T64>(uint seed, Func<uint, T32> factory32, Func<uint, T64> factory64)
+        private static HashAlgorithm Pick<T32, T64>(uint seed, AlgorithmPreference preference, Func<uint, T32> factory32, Func<uint, T64> factory64)
             where T32 : HashAlgorithm
             where T64 : HashAlgorithm
         {
-            if (Environment.Is64BitProcess)
-                return factory64(seed);
+            switch (preference)
+            {
+                case AlgorithmPreference.X64: return factory64(seed);
+                case AlgorithmPreference.X86: return factory32(seed);
+                default:
+                {
+                    if (Environment.Is64BitProcess)
+                        return factory64(seed);
 
-            return factory32(seed);
+                    return factory32(seed);
+                }
+            }
         }
     }
 }
