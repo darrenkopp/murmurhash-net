0076bb988b71230393ec0d8e070248b086eb3691
Darren Kopp <darrenkopp@gmail.com>
2013-10-06 23:03:44 -0600

    support for building nuget package via script

---
+++ release.cmd
@@ -0,0 +1,12 @@
+﻿@echo off
+@SET Framework40Version=4.0
+
+@if "%PathToMsBuild%"=="" (
+  @for /F "tokens=1,2*" %%i in ('reg query "HKLM\SOFTWARE\Wow6432Node\Microsoft\MSBuild\ToolsVersions\%Framework40Version%" /v "MSBuildToolsPath"') DO (
+    if "%%i" == "MSBuildToolsPath" (
+      @SET "PathToMsBuild=%%k"
+    )
+  )
+)
+
+%PathToMsBuild%\MSBuild.exe default.proj /nologo /v:n /m:4
